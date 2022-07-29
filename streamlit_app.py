23dfb80e6955f42f470a72ad6076a8e723ecb4da
prashant1998-eng <58376411+prashant1998-eng@users.noreply.github.com>
2022-07-29 17:34:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
+
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 #will fetch only first row
 #my_data_row = my_cur.fetchone()
 #will fetch all rows
